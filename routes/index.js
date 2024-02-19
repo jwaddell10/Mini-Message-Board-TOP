2aca9c006fe4e7b060d37be724c1a04fc17ad6ca
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-02-19 16:00:41 -0600

    update for question

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
